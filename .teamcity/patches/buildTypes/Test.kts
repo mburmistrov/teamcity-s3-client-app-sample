74296aaeb5a19faec6066125ba275abf56cf6733
admin <admin@TeamCity>
2024-03-04 21:43:05 +0000

    TeamCity change in 'Teamcity S3 Client App Sample' project: runners of 'Test' build configuration were updated

--- .teamcity/patches/buildTypes/Test.kts
+++ .teamcity/patches/buildTypes/Test.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.NodeJSBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.ui.*
 
@@ -10,6 +12,27 @@ To apply the patch, change the buildType with id = 'Test'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Test")) {
+    expectSteps {
+        nodeJS {
+            name = "Test"
+            id = "nodejs_runner_1"
+            shellScript = "npm run test:unit"
+            dockerImage = "node:18.19.1"
+        }
+    }
+    steps {
+        update<NodeJSBuildStep>(0) {
+            clearConditions()
+            shellScript = """
+                if [ -n "%teamcity.build.vcs.branch.TeamcityS3ClientAppSample_HttpsGithubComMburmistrovTeamcityS3clientAppSampleRefsHeadsMain%.tezzzme%" ]; then
+                   npm run test:unit
+                else
+                    echo "Skipping build as the required tag is not present."
+                fi
+            """.trimIndent()
+        }
+    }
+
     triggers {
         add {
             vcs {
