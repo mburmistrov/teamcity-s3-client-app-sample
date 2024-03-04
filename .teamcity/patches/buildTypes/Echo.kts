1e741130ab3f056143a62036147d7959a0a9df8e
admin <admin@TeamCity>
2024-03-04 23:39:45 +0000

    TeamCity change in 'Teamcity S3 Client App Sample' project: runners of 'Echo' build configuration were updated

--- .teamcity/patches/buildTypes/Echo.kts
+++ .teamcity/patches/buildTypes/Echo.kts
@@ -22,12 +22,15 @@ create(DslContext.projectId, BuildType({
         script {
             id = "simpleRunner"
             scriptContent = """
-                last_commit_tag = git describe --tags %build.vcs.number%
-                if [ "${'$'}last_commit_tag" == "tezzzme" ]; then
-                  echo "has tezzzme tag"
-                else
-                  echo "dont have tezzzme tag"
-                fi
+                set last_commit_tag = git describe --tags %build.vcs.number%
+                                echo "123"
+                                echo %last_commit_tag%
+                                echo "321"
+                                if [ %last_commit_tag% == "tezzzme" ]; then
+                                  echo "has tezzzme tag"
+                                else
+                                  echo "dont have tezzzme tag"
+                                fi
             """.trimIndent()
         }
     }
