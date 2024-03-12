e381b8f5ec1a948fd1da91fb775938fc80df857a
admin <admin@TeamCity>
2024-03-12 10:51:20 +0000

    TeamCity change in 'Teamcity S3 Client App Sample' project: VCS roots of 'i18n pull' build configuration were updated

--- .teamcity/patches/buildTypes/I18nPull.kts
+++ .teamcity/patches/buildTypes/I18nPull.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("I18nPull")
     name = "i18n pull"
+
+    vcs {
+        root(RelativeId("HttpsGithubComMburmistrovTeamcityS3clientAppSampleRefsHeadsMain"))
+    }
 }))
 
